9ca42a23a535cde9a2e22cde66fb03e6223672f0
Tiago Gouvêa <tiagoaraujodegouvea@gmail.com>
2018-01-14 15:12:08 -0200

    Habilitando cors

--- src/config/server.js
+++ src/config/server.js
@@ -3,9 +3,11 @@ const port = 3003
 const bodyParser = require('body-parser') //responsável pelo parser do corpo da requisição
 const express = require('express') // Servidor que roda encima do node
 const server = express() // Instancia o express
+const allowCors = require('./cors') //Habilita a permissão de cors - Requisição vir de fonte diferente
 
 server.use(bodyParser.urlencoded({ extend: true})) //Para permitir mais tipos de dados do que padrão
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`BACKEND rodando na porta ${port}.`)
